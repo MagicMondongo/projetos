5e932cb0cf6088229405f271829e16570663cbc2
Charlindo <charlesnoe30@gmail>
2024-05-16 08:29:28 -0300

    charlespro

--- script-basico.js
+++ script-basico.js
@@ -1,75 +1,96 @@
 //O vento DOM é acionado quando o HTML foi completamente carregado
 
 document.addEventListener('DOMContentLoaded', function (){
-//Tipos de dados
-//Stringer: Sequencia de caracter
-//Number: números inteiros ou pontos flutuantes (10.4)
-//BooLean: Retorna dois valores, true ou false
-//object: Estrutura de dados que pode armazenar múltiplos valores
-//NULL: quando esta vazio;
-//array: objeto especial ordenado
-//function: bloco de código reutilizavel
+    //Tipos de dados
+    //Stringer: Sequencia de caracter
+    //Number: números inteiros ou pontos flutuantes (10.4)
+    //BooLean: Retorna dois valores, true ou false
+    //object: Estrutura de dados que pode armazenar múltiplos valores
+    //NULL: quando esta vazio;
+    //array: objeto especial ordenado
+    //function: bloco de código reutilizavel
+    
+    //CONST É usado para declarar uma variável cujo valor é permanente 
+            const teste = 50;
+            
+            //LET é usado para declarar variáveis que os valores podem ser
+            //retribuidos
+            let C = 0;
+            C = 10;
+            console.log("---us guri tao com tudo---")
+           
+            const notaMedia = 3;
+    
+           // if (notaMedia >= 6){
+                //alert("Aluno passou");
+           // } 
+           // else if (notaMedia >= 4 && notaMedia <6){
+                //alert("aluno quase passou");
+           // }
+           // else {
+               // alert("aluno reprovou");
+           // }
+            
+           //----------- Operadores----------
+           
+           // = sinal de atribuição 
+           // == sinal de comparação 
+           // === verifica se os dois são iguais e têm o mesmo tipo de dados
+           // != se é diferente
+           // !== se é estritamente diferente
+           // > maior
+           // <menor
+           
+           //-----------------//
+            
+           //const num = 6;
+           //if (num === 6) {
+            //alert("é igual");
+           //}
+           //else{
+            //alert("O tipo de dado é diferente")
+           //}
+    
+          const aluno = "Charles";
+          if(aluno == "Charles") {
+           console.log("Sim é o Charles")
+          } else{
+            console.log("Não é o Charles")
+          }
 
-//CONST É usado para declarar uma variável cujo valor é permanente 
-        const teste = 50;
-        
-        //LET é usado para declarar variáveis que os valores podem ser
-        //retribuidos
-        let C = 0;
-        C = 10;
-        //console.log("---us guri tao com tudo---")
-       
-        //const notaMedia = 3;
+          //------------------//
 
-        //if (notaMedia >= 6){
-            //alert("Aluno passou");
-       //} 
-        //else if (notaMedia >= 4 && notaMedia <6){
-           // alert("aluno quase passou");
-        //}
-        //else {
-            //alert("aluno reprovou");
-        //}
-        
-       //----------- Operadores----------
-       
-       // = sinal de atribuição 
-       // == sinal de comparação 
-       // === verifica se os dois são iguais e têm o mesmo tipo de dados
-       // != se é diferente
-       // !== se é estritamente diferente
-       // > maior
-       // <menor
-        
-       //const num = 6;
-       //if (num === 6) {
-        //alert("é igual");
-       //}
-       //else{
-       // alert("O tipo de dado é diferente")
-       //}
+          function primeiraFuncao(parametro) {
+            return parametro + segundoParametro;
+          }
 
-      //const aluno = "Charles";
-      //if(aluno == "Charles") {
-       //console.log("Sim é o Charles")
-      //} else{
-        //console.log("Não é o Charles")
-      //}
+          const parametro = "essa é uma função com parametro"
+          const segundoParametro = ", agora com dois parametros"
 
+          console.log(primeiraFuncao(parametro, segundoParametro));
 
+          //------------------//
 
-      //Funções são blocos de código que podem ser chamados e reutilizados
-      //Dentro dos parenteses da função vai os parâmetros
+          //arrays em javascript são estruturas de dados que permitem 
+          //armazenar uma coleçao ordenada de elementos.
+          //const é permanente
+          //let pode mudar não é permanente
+          //--------------//
 
-      function primeiraFunção(parametro, segundoParametro) {
+         let primeiroArray = ['Charles', 'Otavio','Igor', 'João'];
 
-        return parametro + segundoParametro;
+         console.log(primeiroArray[0])
+         //---------------------//
 
-      }
-      
-      const parametro = "Essa é uma função com parãmetro"
-      const segundoParametro = ", agora com dois parametros"
+         //Os objetos são coleções NÃO ORDENADAS de
+         //pares chave-valor, onde cada valor é associado a uma chave única
 
-      console.log(primeiraFunção(parametro, segundoParametro));
- 
-});
+         let pessoa = {
+             nome: 'Charles',
+             idade: '18',
+             profissao: 'Gostoso'
+         }
+
+         console.log(pessoa['profissao']);
+    
+    });
